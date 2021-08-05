b339c9dfddb090c467ebe5e95e949513d0176674
alexei matlash <alexei.matlash@yandex.ru>
2021-08-05 05:19:16 +0300

    vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const resolvePath = (filePath: string) => path.resolve(__dirname, filePath);
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [
+        vue()
+    ],
+    resolve: {
+        alias: {
+            '~/': `${resolvePath('src')}/`,
+        }
+    },
+    server: {
+        host: true
+    }
 })
