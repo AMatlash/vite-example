0b91c0e83a47caa6708d66ec258cd71e8dc80b18
alexei matlash <alexei.matlash@yandex.ru>
2021-08-05 05:56:43 +0300

    @vueuse/head

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,10 @@
 
 <script setup lang="ts">
     import { useI18n } from 'vue-i18n';
+    import { useHead } from '@vueuse/head';
+
     const { t } = useI18n();
+    useHead({
+        title: 'Vite example',
+    });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createHead } from '@vueuse/head';
 import { i18n } from '~/i18n';
+import App from './App.vue'
 
 createApp(App)
     .use(i18n)
+    .use(createHead())
     .mount('#app')
