e8642bf8810b0b65e095f70c89c5288c79e1b72f
Aleksei Matlash <alexei.matlash@yandex.ru>
2021-08-05 11:28:26 +0300

    ts vuex test

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { createHead } from '@vueuse/head';
 import { i18n } from '~/i18n';
 import router from '~/router';
 import App from './App.vue';
+import { store, key } from '~/store';
 import '~/styles/index.css';
 
 createApp(App)
     .use(i18n)
+    .use(store, key)
     .use(router)
     .use(createHead())
     .mount('#app');
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-    [Index page]
+    [Index page] {{ store.state.count }}
 </template>
+
+<script lang="ts">
+    import { useStore } from '~/store';
+
+    export default {
+        setup () {
+            const store = useStore();
+            console.log(store);
+            store.commit('increment', '120');
+            console.log(store.state.count);
+            return { store };
+        }
+    };
+</script>
